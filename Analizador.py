b6ea11409e1937cbaad6a6fed3684891432f644e
JerrySC68 <jerrysc68@gmail.com>
2023-10-29 20:52:08 -0600

    Boceto de Analizador

--- Analizador.py
+++ Analizador.py
@@ -3,5 +3,25 @@
 class Analizador:
     def __init__(self):
         self.tabla_simbolos = {}
+ 
+    #Verificamos si la variable es tipo int, float, void, string#
+    def validar_variable:
+        
 
+    #Agrega la variable a la tabla de simbolos, antes se tiene que validar la variable#
+    def agregar_variable:
+        
+    
+    #Verificamos si la funcion es tipo int, float, void, string#
+    def validar_funcion: 
+    
+
+    #Agrega la funcion a la tabla de simbolos, antes se tiene que validar la variable#
+    def agregar_funcion:
+    
+
+    #def validar_contenido_funcion posible método a desarrollar#
+   
+
+        
     
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proyecto2_Estructuras
 Proyecto #2 de Estructuras de Datos.  Analizador Semántico
+- El archivo codigo_fuente.txt es donde colocamos el código en el cual pondremos a prueba nuestra tabla hash,
+para ver si es capaz de analizar distintos códigos, y ver cuales funcionan y cuales no.
