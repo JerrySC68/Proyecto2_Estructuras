b1c4f4ee4b07f1021edeaa594357a1de2e8b4d15
JerrySC68 <jerrysc68@gmail.com>
2023-10-29 21:14:27 -0600

    Update Analizador.py

---
+++ Analizador.py
@@ -0,0 +1,35 @@
+
+
+class Analizador:
+    def __init__(self):
+        self.tabla_simbolos = {}
+
+    def leer_archivo_texto(nombre_archivo):
+        try:
+            with open(nombre_archivo, 'r') as archivo:
+                text = archivo.read()
+            return text
+        except FileNotFoundError:
+            print(f"El archivo '{nombre_archivo}' no se encontró.")
+            return None
+    #Verificamos si la variable es tipo int, float, void, string#
+    def validar_variable:
+        
+
+    #Agrega la variable a la tabla de simbolos, antes se tiene que validar la variable#
+    def agregar_variable:
+        
+    
+    #Verificamos si la funcion es tipo int, float, void, string#
+    def validar_funcion: 
+    
+
+    #Agrega la funcion a la tabla de simbolos, antes se tiene que validar la variable#
+    def agregar_funcion:
+    
+
+    #def validar_contenido_funcion posible método a desarrollar#
+   
+
+        
+    
