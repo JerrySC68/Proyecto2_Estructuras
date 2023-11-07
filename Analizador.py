762b720a3e344782e02d778855b05efc52803525
JerrySC68 <jerrysc68@gmail.com>
2023-11-07 15:53:02 -0600

    Update Analizador.py

--- Analizador.py
+++ Analizador.py
@@ -20,7 +20,8 @@ class Analizador:
     def tokenize(self, codigo_fuente):
         if self.leer_archivo_texto(codigo_fuente) is not None:
             tokens = re.findall(r'\b\w+\b', codigo_fuente)
-        return tokens
+            return tokens
+        return None
         
     #Verificamos si la variable es tipo int, float, void, string#
     #def validar_variable:
@@ -37,21 +38,22 @@ class Analizador:
         nombre_actual = None   # Nombre de la función#
         dentro_funcion = False # Nos indica si estamos dentro de los parámetros de la función (dentro de las {})#
 
-        for i, token in enumerate(tokens):
-            if token in ['int','float','string','void']: # Si nos encontramos algunas de estos 4 palabras se refieren al tipo de función y lo guardamos en tipo_actual#
-                tipo_actual = token
-            elif token == '{': #Agregar contador para verificar cantidad de llaves abiertas para ver las llaves cerradas.
-                if nombre_actual and tipo_actual:
-                    self.tabla_simbolos[nombre_actual] = tipo_actual
-                tipo_actual = None
-                nombre_actual = None
-                dentro_funcion = True
-            elif token == '}':
-                dentro_funcion = False
-            elif self.tipo_funcion and token == 'return':
-                return_tipo = tokens[i + 1]
-                if return_tipo != self.tipo_funcion:
-                    print(f"Error - Línea {i + 1}: Valor de retorno no coincide con la declaración de la función")
+        if tokens is not None:
+            for i, token in enumerate(tokens):
+                if token in ['int','float','string','void']: # Si nos encontramos algunas de estos 4 palabras se refieren al tipo de función y lo guardamos en tipo_actual#
+                    tipo_actual = token
+                elif token == '{': #Agregar contador para verificar cantidad de llaves abiertas para ver las llaves cerradas.
+                    if nombre_actual and tipo_actual:
+                        self.tabla_simbolos[nombre_actual] = tipo_actual
+                    tipo_actual = None
+                    nombre_actual = None
+                    dentro_funcion = True
+                elif token == '}':
+                    dentro_funcion = False
+                elif self.tipo_funcion and token == 'return':
+                    return_tipo = tokens[i + 1]
+                    if return_tipo != self.tipo_funcion:
+                        print(f"Error - Línea {i + 1}: Valor de retorno no coincide con la declaración de la función")
 
 
 
