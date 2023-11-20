672fab28509e9460e6a5845ec2193e4ab7082bc6
JerrySC68 <jerrysc68@gmail.com>
2023-11-19 22:23:16 -0600

    Creo el main

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import palabrasReservadas
-import Analizador
 
 """
 Estructuras de Datos
@@ -9,4 +7,23 @@ Reyder Fernández
 John Colomer
 
 """
+
+import Analizador
+import palabrasReservadas
+
+if __name__ == '__main__':
+    print("Código normal\n")
+    Tabla_1 = Analizador.TablaSimbolos()
+    Tabla_1.leeArchivo("codigo_fuente.txt")
+    Tabla_1.imprimirFuncion()
+    Tabla_1.muestraErrores()
+
+    print("\n\n\n\n\n\n")
+
+    print("Código con errores\n")
+    Tabla_2 = Analizador.TablaSimbolos()
+    Tabla_2.leeArchivo("codigo_fuente_con_error.txt")
+    Tabla_2.imprimirFuncion()
+    Tabla_2.muestraErrores()
+
 
