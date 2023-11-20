0d0ed54d28b840fe7df2eb28110b98478caced1b
PELOTALOL <reyderfm@gmail.com>
2023-11-19 22:37:02 -0600

    Correccion de errores

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import palabrasReservadas
 
 if __name__ == '__main__':
     print("Código normal\n")
-    Tabla_1 = Analizador.TablaSimbolos()
+    Tabla_1 = Analizador.Analizador()
     Tabla_1.leeArchivo("codigo_fuente.txt")
     Tabla_1.imprimirFuncion()
     Tabla_1.muestraErrores()
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     print("\n\n\n\n\n\n")
 
     print("Código con errores\n")
-    Tabla_2 = Analizador.TablaSimbolos()
+    Tabla_2 = Analizador.Analizador()
     Tabla_2.leeArchivo("codigo_fuente_con_error.txt")
     Tabla_2.imprimirFuncion()
     Tabla_2.muestraErrores()
